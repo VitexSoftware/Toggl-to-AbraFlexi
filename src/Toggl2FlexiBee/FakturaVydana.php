b9b8fb6d2da5a5f34fb921f3727903cf33d0788b
Vítězslav Dvořák <info@vitexsoftware.cz>
2020-09-04 09:55:36 +0200

    Code cleanup

--- src/Toggl2FlexiBee/FakturaVydana.php
+++ src/Toggl2FlexiBee/FakturaVydana.php
@@ -9,46 +9,57 @@ use Ease\Shared;
  *
  * @author Vítězslav Dvořák <info@vitexsoftware.cz>
  */
-class FakturaVydana extends \FlexiPeeHP\FakturaVydana
-{
+class FakturaVydana extends \FlexiPeeHP\FakturaVydana {
 
     /**
      * FlexiBee Invoice
      *
      * @param array $options Connection settings override
      */
-    public function __construct($init = null, $options = [])
-    {
+    public function __construct($init = null, $options = []) {
         parent::__construct($init, $options = []);
         if (!array_key_exists('typDokl', $init)) {
             $this->setDataValue('typDokl',
-                self::code(Shared::instanced()->getConfigValue('FLEXIBEE_TYP_FAKTURY')));
+                    self::code(Shared::instanced()->getConfigValue('FLEXIBEE_TYP_FAKTURY')));
         }
+    }
+
+    public static function formatMilliseconds($milliseconds) {
+        $seconds = floor($milliseconds / 1000);
+        $minutes = floor($seconds / 60);
+        $hours = floor($minutes / 60);
+        $milliseconds = $milliseconds % 1000;
+        $seconds = $seconds % 60;
+        $minutes = $minutes % 60;
+
+        $format = '%u:%02u:%02u.%03u';
+        $time = sprintf($format, $hours, $minutes, $seconds, $milliseconds);
+        return rtrim($time, '0');
     }
 
     /**
      * 
      * @param array $timeEntries
      */
-    public function takeItemsFromArray($timeEntries)
-    {
+    public function takeItemsFromArray($timeEntries) {
         $itemsData = [];
 
         foreach ($timeEntries as $projectName => $projectTimeEntries) {
-            $this->addArrayToBranch(['typPolozkyK' => 'typPolozky.text', 'nazev' => 'Projekt: '.$projectName],
-                'polozkyFaktury'); // Task Title as Heading/TextRow
+            $this->addArrayToBranch(['typPolozkyK' => 'typPolozky.text', 'nazev' => 'Projekt: ' . $projectName],
+                    'polozkyFaktury'); // Task Title as Heading/TextRow
 
             foreach ($projectTimeEntries as $nazev => $duration) {
 
                 $taskData = [
 //                            'id' => 'ext:redmine:'.$rowId,
                     'typPolozkyK' => 'typPolozky.katalog',
-                    'nazev' => $nazev,
-                    'mnozMj' => round(($duration / 1000) / 3600, 3) ,
+                    'nazev' => self::formatMilliseconds($duration) . ' '. $nazev,
+                    'mnozMj' => round($duration / 3600000, 3),
                     'cenik' => self::code(Shared::instanced()->getConfigValue('FLEXIBEE_CENIK'))];
 
                 $this->addArrayToBranch($taskData, 'polozkyFaktury');
             }
         }
     }
+
 }
--- src/invoicer.php
+++ src/invoicer.php
@@ -5,7 +5,6 @@ namespace Toggl2FlexiBee;
 use Ease\Locale;
 use Ease\Shared;
 use Ease\TWB4\WebPage;
-use Ease\WebPage as WebPage2;
 use AJT\Toggl\ReportsClient;
 use AJT\Toggl\TogglClient;
 
@@ -19,28 +18,33 @@ $oPage = new WebPage('Toggl2FlexiBee: Invoice');
 
 $apiKey = Shared::instanced()->getConfigValue('TOGGLE_TOKEN');
 
-$toggl_client = TogglClient::factory(array('api_key' => $apiKey));
+$toggl_client = TogglClient::factory(['api_key' => $apiKey]);
 
 $me = $toggl_client->getCurrentUser(['with_related_data' => true])->toArray();
 
-$toggl_client = ReportsClient::factory(array('api_key' => $apiKey, 'debug' => false));
-$detailsData  = $toggl_client->Details([
-        'since' => WebPage2::getRequestValue('since'),
-        'until' => WebPage2::getRequestValue('until'),
-        'display_hours' => 'decimal',
-        'workspace_id' => WebPage2::getRequestValue('workspace', 'int'),
-        'user_agent' => constant('EASE_APPNAME'),
-        'user_ids' => $me['data']['id']
-    ])->toArray();
+$toggl_client = ReportsClient::factory(['api_key' => $apiKey, 'debug' => false]);
+$detailsData = $toggl_client->Details([
+            'since' => WebPage::getRequestValue('since'),
+            'until' => WebPage::getRequestValue('until'),
+            'display_hours' => 'decimal',
+            'workspace_id' => WebPage::getRequestValue('workspace', 'int'),
+            'user_agent' => \Ease\Functions::cfg('EASE_APPNAME'),
+            'user_ids' => $me['data']['id'],
+        ])->toArray();
 
 $invoiceItems = [];
+$projects = [];
+$durations = [];
+
 
 
 foreach ($detailsData['data'] as $detail) {
 
-    $project  = $detail['project'];
-    $task     = $detail['description'];
-    $duration = $detail['dur'] ;
+    $project = empty($detail['project']) ? _('No Project') : $detail['project'];
+    $task = $detail['description'];
+    $duration = $detail['dur'];
+
+    $durations[] = FakturaVydana::formatMilliseconds($duration) . ' ' . $project . ' ' . $task;
 
     if (!array_key_exists($project, $invoiceItems)) {
         $invoiceItems[$project] = [];
@@ -49,32 +53,39 @@ foreach ($detailsData['data'] as $detail) {
         $invoiceItems[$project][$task] = 0;
     }
     $invoiceItems[$project][$task] += $duration;
+    $projects[$project] = $project;
 }
 
-
-echo '<pre>';
-
-print_r($invoiceItems);
-
-echo '</pre>';
+//
+//echo '<pre>';
+//
+//echo count($detailsData['data']);
+//
+//print_r($durations);
+//
+//
+//print_r($invoiceItems);
+//
+//echo '</pre>';
 
 $invoicer = new FakturaVydana([
     'typDokl' => \FlexiPeeHP\FlexiBeeRO::code('FAKTURA'),
     'firma' => \FlexiPeeHP\FlexiBeeRO::code($oPage->getRequestValue('firma') ? current($oPage->getRequestValue('firma')) : Shared::instanced()->getConfigValue('FLEXIBEE_CUSTOMER')),
-    'popis' => sprintf(_('Work from %s to %s'), WebPage2::getRequestValue('since'), WebPage2::getRequestValue('until'))
-    ]);
+    'popis' => sprintf(_('Work from %s to %s'), WebPage::getRequestValue('since'), WebPage::getRequestValue('until')),
+    'poznam' => 'Toggl Workspace: ' . WebPage::getRequestValue('workspace', 'int')
+        ]);
 
 
 
 $invoicer->takeItemsFromArray($invoiceItems);
 $created = $invoicer->sync();
 
-$invoiceTabs = new \Ease\TWB4\Tabs(null,['id'=>'Invoices']);
+$invoiceTabs = new \Ease\TWB4\Tabs(null, ['id' => 'Invoices']);
 
-$invoiceTabs->addTab(_('Html'),new \FlexiPeeHP\ui\EmbedResponsiveHTML($invoicer));
+$invoiceTabs->addTab(_('Html'), new \FlexiPeeHP\ui\EmbedResponsiveHTML($invoicer));
 $invoiceTabs->addTab(_('PDF'), new \FlexiPeeHP\ui\EmbedResponsivePDF($invoicer));
 
-$oPage->addItem(new \Ease\TWB4\Container(new \Ease\TWB4\Panel('Doklad '.new \Ease\Html\ATag($invoicer->getApiUrl(),
-                $invoicer->getDataValue('kod')).' '.($created ? 'byl' : 'nebyl').' vystaven',
-            $created ? 'success' : 'danger', $invoiceTabs            )));
+$oPage->addItem(new \Ease\TWB4\Container(new \Ease\TWB4\Panel('Doklad ' . new \Ease\Html\ATag($invoicer->getApiUrl(),
+                                $invoicer->getDataValue('kod')) . ' ' . ($created ? 'byl' : 'nebyl') . ' vystaven',
+                        $created ? 'success' : 'danger', $invoiceTabs)));
 $oPage->draw();
